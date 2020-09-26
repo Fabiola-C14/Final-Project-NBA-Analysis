f5116ab2772c5f20c9dcb336f93a933ff353d871
Christian Pompa <pompa.chris@gmail.com>
2020-09-26 13:32:55 -0700

    fixed plotly displaying in all stars

--- static/js/jane.js
+++ static/js/jane.js
@@ -50,7 +50,7 @@
 const top_n = 12;
 const height = 600;
 const width = 1000;
-    
+
 const margin = {
     top: 80,
     right: 50,
@@ -58,88 +58,94 @@ const margin = {
     left: 50
 };
 
-let barPadding = (height-(margin.bottom+margin.top))/(top_n*5);
+let barPadding = (height - (margin.bottom + margin.top)) / (top_n * 5);
 
 //Create svg wrapper to append into to hold chart  
-const svg = d3.select("#content").append("svg")
+const svg = d3.selectAll("#content").append("svg")
     .attr("width", 1000)
     .attr("height", 600);
 
 function makeplot() {
-    Plotly.d3.csv('../../static/datasets/all_stars_yes.csv', function(data){
-    Plotly.d3.csv('../../static/datasets/all_stars_no.csv', function(data2){
-    
-        var x1 = [], y1 = [], m1 = [], p1 = [];
-        var x2 = [], y2 = [], m2 = [], p2 = [];
-        
-        for (var i=0; i<data.length; i++) {
-            row = data[i];
-            x1.push( row['Year'] );
-            y1.push( row['Inflation'] );
-            m1.push( row['Selected?'] );
-            p1.push( row['Player'] );
-            // n1.push( row['Prior ASG Appearances'] );
-        }
-        
-        for (var i=0; i<data2.length; i++) {
-            row = data2[i];
-            x2.push( row['Year'] );
-            y2.push( row['Inflation'] );
-            m2.push( row['Selected?'] );
-            p2.push( row['Player'] );
-            // n2.push( row['Prior ASG Appearances'] );
-        }
-        
-        var trace1 = [{
-            x: x1, 
-            y: y1,
-            name: "All Stars",
-            mode: 'markers',
-            type: 'scatter',
-            hovertemplate: 'Player: %{text}' +
-                        '<br>Salary: $%{y}<br>', 
-            text: p1, 
-            marker: {
-                color: '#ffbe26',
-                size: 8,
-                line: {
-                    color: '#0a1b29',
-                    width: 1
-                },               
-            }
-        
-        }];
-        
-        var trace2 = [{
-            x: x2, 
-            y: y2,
-            name: "Not All Stars",
-            mode: 'markers',
-            type: 'scatter',
-            hovertemplate: 'Player: %{text}' +
-                        '<br>Salary: $%{y}<br>', 
-            text: p2, 
-            marker: {
-                color: '#157a79',
-                size: 5,
-                line: {
-                    color: '#0a1b29',
-                    width: 1
-                }
+    Plotly.d3.csv('./static/datasets/all_stars_yes.csv', function(data) {
+        Plotly.d3.csv('./static/datasets/all_stars_no.csv', function(data2) {
+            console.log('test');
+            var x1 = [],
+                y1 = [],
+                m1 = [],
+                p1 = [];
+            var x2 = [],
+                y2 = [],
+                m2 = [],
+                p2 = [];
+
+            for (var i = 0; i < data.length; i++) {
+                row = data[i];
+                x1.push(row['Year']);
+                y1.push(row['Inflation']);
+                m1.push(row['Selected?']);
+                p1.push(row['Player']);
+                // n1.push( row['Prior ASG Appearances'] );
             }
 
-        }];
-        
-        var layout = {
-            title: 'All Stars vs Salary',
-            hovermode: 'closest'       
-        };
-        
-        var AllData =[trace1[0], trace2[0]];			
-        Plotly.newPlot('myDiv', AllData, layout);	
-        
-    }); 
+            for (var i = 0; i < data2.length; i++) {
+                row = data2[i];
+                x2.push(row['Year']);
+                y2.push(row['Inflation']);
+                m2.push(row['Selected?']);
+                p2.push(row['Player']);
+                // n2.push( row['Prior ASG Appearances'] );
+            }
+
+            var trace1 = [{
+                x: x1,
+                y: y1,
+                name: "All Stars",
+                mode: 'markers',
+                type: 'scatter',
+                hovertemplate: 'Player: %{text}' +
+                    '<br>Salary: $%{y}<br>',
+                text: p1,
+                marker: {
+                    color: '#ffbe26',
+                    size: 8,
+                    line: {
+                        color: '#0a1b29',
+                        width: 1
+                    },
+                }
+
+            }];
+
+            var trace2 = [{
+                x: x2,
+                y: y2,
+                name: "Not All Stars",
+                mode: 'markers',
+                type: 'scatter',
+                hovertemplate: 'Player: %{text}' +
+                    '<br>Salary: $%{y}<br>',
+                text: p2,
+                marker: {
+                    color: '#157a79',
+                    size: 5,
+                    line: {
+                        color: '#0a1b29',
+                        width: 1
+                    }
+                }
+
+            }];
+
+            var layout = {
+                title: 'All Stars vs Salary',
+                hovermode: 'closest'
+            };
+
+            var AllData = [trace1[0], trace2[0]];
+            Plotly.newPlot('content', AllData, layout);
+
+        });
     });
-    };
-    
-    makeplot();
+};
+
+makeplot();
